813deb91234c9d944b151e104d325df1e4d74e84
hellofish <stonefavorite@126.com>
2017-07-29 16:09:28 +0800

    add 本地提交配置

--- sc-portal/src/main/java/com/sc/interceptor/CheckUserInterceptor.java
+++ sc-portal/src/main/java/com/sc/interceptor/CheckUserInterceptor.java
@@ -30,19 +30,16 @@ public class CheckUserInterceptor implements HandlerInterceptor{
 	@Value("${TICKET_COOKIE_NAME}")
 	private String TICKET_COOKIE_NAME;
 	
-	@Override
 	public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3)
 			throws Exception {
 		
 	}
 
-	@Override
 	public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, ModelAndView arg3)
 			throws Exception {
 		
 	}
 
-	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object arg2) throws Exception {
 		String ticket = CookieUtils.getCookieValue(request, TICKET_COOKIE_NAME);
 		Jedis jedis = jedisUtils.getJedis();
